4c46bf93f0d7c06ef3d193bff219c9474aa91195
ele828 <dongjiemusic@qq.com>
2016-08-14 23:22:01 +0800

    fix indent

--- src/decorator/Column.ts
+++ src/decorator/Column.ts
@@ -2,31 +2,31 @@ import {ColumnTypes} from '../type';
 import {entityCollection, Entity, Property} from '../collection'; 
 
 export function Column(typeOrOptions?, options?): Function {
-    return function (object: Object, propertyName: string) {
+  return function (object: Object, propertyName: string) {
+    
+    const entityName = object.constructor.name;
+    const propertyType = Reflect.getMetadata("design:type", object, propertyName).name;
 
-      const entityName = object.constructor.name;
-      const propertyType = Reflect.getMetadata("design:type", object, propertyName).name;
+    let type: ColumnTypes = getPropertyType(propertyType);
+    let entityExist: boolean = entityCollection.has(entityName)? true: false;
 
-      let type: ColumnTypes = getPropertyType(propertyType);
-      let entityExist: boolean = entityCollection.has(entityName)? true: false;
+    let metadata: Property = {
+        propertyName: propertyName,
+        propertyType: type
+    };
 
-      let metadata: Property = {
-          propertyName: propertyName,
-          propertyType: type
-      };
-
-      if (entityExist) {
-        let entity = entityCollection.find(entityName) as Entity<Property>;
-        entity.insert(metadata);
-      }
-      else {
-        let entity = new Entity(entityName);
-        entity.insert(metadata);
-        // insert to entityCollection
-        entityCollection.insert(entity);
-      }
-
+    if (entityExist) {
+      let entity = entityCollection.find(entityName) as Entity<Property>;
+      entity.insert(metadata);
     }
+    else {
+      let entity = new Entity(entityName);
+      entity.insert(metadata);
+      // insert to entityCollection
+      entityCollection.insert(entity);
+    }
+
+  }
 }
 
 function getPropertyType(propertyType: string): ColumnTypes {
