a75c4471bb5c585156fe486c49d384456a94e509
ele828 <dongjiemusic@qq.com>
2016-08-17 13:55:13 +0800

    [Update] Query API

--- src/core/Query.ts
+++ src/core/Query.ts
@@ -60,28 +60,83 @@ interface QueryApi<E extends Model> {
  * @template E
  */
 export class Query<E extends Model> implements QueryApi<E> {
+
+  /**
+   * Sequelize entity reference
+   * 
+   * @private
+   * @type {E}
+   */
   private _clazz: E;
+
+  /**
+   * Sequelize attribute collections
+   * 
+   * @private
+   * @type {Array<any>}
+   */
+  private _attributes: Array<any>;
+
+  /**
+   * Sequelize where collections
+   * 
+   * @private
+   * @type {Array<any>}
+   */
+  private _where: Array<any>;
 
   constructor(clazz: E) {
     this._clazz = clazz;
   }
 
+  /**
+   * Count number in specify conditions
+   * [sequelize.fn('COUNT', sequelize.col('hats')), 'no_hats']
+   * 
+   * @param {string} column
+   * @param {string} [alias]
+   * @returns {Query<E>}
+   */
   public count(name: string, alias?: string): Query<E> {
     throw 'Not Implemented';
   }
 
+   /**
+   * Complex query conditions 
+   * 
+   * @param {{}} conditions
+   * @returns {Query<E>}
+   */
   public cond(conditions: {}): Query<E> {
     throw 'Not Implemented';
   }
 
+  /**
+   * Exclude specified columns
+   * 
+   * @returns {Query<E>}
+   */
   public not(name: string): Query<E> {
     throw 'Not Implemented';
   }
 
+  /**
+   * Specify query columns.
+   * If invoked, findAll will just query this specified columns instead of query all.
+   * 
+   * @param {string} name
+   * @param {string} [alias]
+   * @returns {Query<E>}
+   */
   public column(name: string, alias?: string): Query<E> {
     throw 'Not Implemented';
   }
 
+  /**
+   * Build all conditions, and executes findAll operation
+   * 
+   * @returns {Result<E>}
+   */
   public findAll(): Result<E> {
     throw 'Not Implemented';
   }
